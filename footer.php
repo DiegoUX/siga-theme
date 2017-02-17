f7aacede25736986444bfb95e067858b37d048cc
Diego Herrando <diegonzah@gmail.com>
2017-02-17 13:07:09 -0300

    footer y ajustes responsive home

--- footer.php
+++ footer.php
@@ -1,12 +1,48 @@
 			<!-- footer -->
-			<footer class="footer" role="contentinfo">
-
-				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
-					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
-				</p>
-				<!-- /copyright -->
+			<footer class="footer section" id="contact" role="contentinfo">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12 contact-title">
+							<h2>Contacto</h2>
+							<h4>¿Tenés una consulta? Escribinos y contanos, respondemos a la brevedad.</h4>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-lg-4 col-md-5 info-contacto">
+							<a href="#"><img src="../wp-content/themes/siga-theme/img/logo-invert.svg" alt="Logo Siga Proyungas"></a>
+							<h5>Sistema de Información<br>Geográfica Ambiental</h5>
+							<p>Perú 1180 • Yerba Buena</p>
+							<p>4107 • Tucumán • Argentina</p>
+							<p class="tel">Tel./Fax: +54 381 425 3728</p>
+							<a href="mailto:siga@proyungas.org.ar" >siga@proyungas.org.ar</a>
+						</div>
+						<div class="col-lg-4 col-md-7 form-contacto">
+							<?php echo do_shortcode('[contact-form-7 id="48" title="contacto"]'); ?>
+						</div>
+						<div class="col-lg-3 offset-lg-1 servicio-contacto">
+							<img src="../wp-content/themes/siga-theme/img/soporte.svg" alt="Icono Soporte Técnico">
+							<p>¿Error en una descarga?<br>Comunicate directamente<br> con el equipo técnico: </p>
+							<a href="mailto:soporte.siga@proyungas.org.ar">soporte.siga@proyungas.org.ar</a>
+						</div>
+					</div>
+				</div>
+				
+				<div class="bottom-foo">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-6">
+								<!-- copyright -->
+								<p class="copyright">
+									&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>
+								</p>
+								<!-- /copyright -->
+							</div>	
+							<div class="col-md-6 credit">
+								<p>Este sitio es una creación de  <a href="http://tapirus.com.ar" target="_blank" class="tcolor">T a p i r u s</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
 
 			</footer>
 			<!-- /footer -->
--- home-page.php
+++ home-page.php
@@ -41,29 +41,29 @@
 						<div class="row">
 							<div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1">
 								<h2>Nuestros Servicios</h2>
-								<h4>Generamos y análizamos información del subtrópico de Argentina, para luego difundir la cartográfica. Ofrecemos capacitación técnica en distintos niveles educativos. Desarrollamos proyectos con incidencia sobre políticas ambientales y productivas.</h4>
+								<h4>Generamos y analizamos información espacial del Subtrópico y la difundimos en mapas. Ofrecemos capacitación técnica en distintos niveles educativos. Participamos en proyectos con incidencia en políticas ambientales y productivas.</h4>
 							</div>
 						</div>
 						<div class="row">
-							<div class="col-sm-3">
+							<div class="col-lg-3 col-sm-6 mbl-mgn two-top-services">
 								<img src="../wp-content/themes/siga-theme/img/visores.svg" alt="Icon Visores de Mapas">
 								<h3>Visores<br> de Mapas</h3>
 								<p>Visualizá la información en capas, descargá documentos y accedé a mapas interactivos.</p>
 								<a href="#" class="btn btn-main">Ver Más</a>
 							</div>
-							<div class="col-sm-3">
+							<div class="col-lg-3 col-sm-6 mbl-mgn two-top-services">
 								<img src="../wp-content/themes/siga-theme/img/appmoviles.svg" alt="Icon Apps Móviles">
 								<h3>Aplicaciones<br>Móviles</h3>
 								<p>Visualizá la información en capas, descargá documentos y accedé a mapas interactivos.</p>
 								<a href="#" class="btn btn-main">Ver Más</a>
 							</div>
-							<div class="col-sm-3">
+							<div class="col-lg-3 col-sm-6 mbl-mgn">
 								<img src="../wp-content/themes/siga-theme/img/cartografia.svg" alt="Icon Produtos Cartográficos">
 								<h3>Productos<br> Cartográficos</h3>
 								<p>Visualizá la información en capas, descargá documentos y accedé a mapas interactivos.</p>
 								<a href="#" class="btn btn-main">Ver Más</a>
 							</div>
-							<div class="col-sm-3">
+							<div class="col-lg-3 col-sm-6 mbl-mgn">
 								<img src="../wp-content/themes/siga-theme/img/capacitacion.svg" alt="Icon Capacitación y Formación">
 								<h3>Formación<br> y Capacitación</h3>
 								<p>Visualizá la información en capas, descargá documentos y accedé a mapas interactivos.</p>
@@ -75,11 +75,11 @@
 				<section class="help-you section">
 					<div class="container">
 						<div class="row">
-							<div class="col-sm-9">
+							<div class="col-md-9">
 								<h2>Podemos ayudarte con algún proyecto?</h2>
 								<h4>Queremos aportar toda nuestra experiencia y capacidad a tu proyecto. Trabajemos en equipo para lograr mejores resultados ¡Contanos de qué se trata!</h4>
 							</div>
-							<div class="col-sm-3 tac">
+							<div class="col-md-3 tac">
 								<a href="#" class="btn btn-invert">Contactanos</a>
 							</div>
 						</div>
@@ -88,11 +88,11 @@
 				<section class="recursos section">
 					<div class="container">
 						<div class="row">
-							<div class="col-sm-9">
+							<div class="col-md-9">
 								<h2>Descargá recursos gratuitos</h2>
 								<h4>Accedé de forma gratuita a mapas y archivos GIS del subtrópico de Argentina. Optimizá tu proyecto con información precisa y actualizada de la región.</h4>
 							</div>
-							<div class="col-sm-3 tac">
+							<div class="col-md-3 tac">
 								<a href="#" class="btn btn-invert">Recursos</a>
 							</div>
 						</div>
@@ -103,9 +103,9 @@
 				
 
 
-				<br class="clear">
+				<!-- <br class="clear"> -->
 
-				<?php edit_post_link(); ?>
+				<!-- <?php edit_post_link(); ?> -->
 
 			</article>
 			<!-- /article -->
